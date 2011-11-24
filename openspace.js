59550f3110a39d73c56a5b5bddaa9f6f0ebb666d
Jonathan Bowers <jonotron@gmail.com>
2011-11-23 19:34:18 -0800

    Basic socket communication

--- openspace.js
+++ openspace.js
@@ -7,4 +7,17 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
+var theShip = {
+  position: {x:0, y:0, z:0},
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('openspace.welcome', {msg: 'Welcome to OpenSpace'});
+
+  socket.on('ship.thrust', function(ship) {
+    theShip.position.x++;
+    theShip.position.y++;
+    socket.emit('position.set', theShip);
+  });
+});
 app.listen(7814);
